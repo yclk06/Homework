b9dcc7a537685680eb0d46cfd9b3807efefe9337
yclk06 <yasincan74@outlook.de>
2024-10-24 05:02:04 +0200

    Create test.java

---
+++ test.java
@@ -0,0 +1,8 @@
+// Your First Program
+
+class test {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("My name is Yasin!"); 
+    }
+}
